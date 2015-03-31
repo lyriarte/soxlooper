0e86a6fd73d55a3393fa2e845949b4256e3a15d4
Luc Yriarte <lyriarte@gmail.com>
2015-03-31 17:54:05 +0200

    loop / silence players in subprocesses + threads to open write fifo without locking

--- soxlooper.py
+++ soxlooper.py
@@ -17,7 +17,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
-import os, subprocess, sys
+import os, subprocess, sys, thread
 
 
 #### #### FIFO management
@@ -37,6 +37,17 @@ soxGlobalOptions = ["--combine", "mix-power"]
 soxFormatOptions = ["--rate", "44100", "--encoding", "signed", "--bits", "16", "--channels", "2"]
 soxOutfile = ["--default"]
 
+soxPlayers = []
+
+def soxPlayLoop(channel, prefix, loopFileName):
+	fifo = os.open(prefix + str(channel), os.O_WRONLY)
+	soxPlayers[channel] = subprocess.Popen(["sox", loopFileName, "-p", "repeat", "-"], stdout=fifo)
+
+def soxPlaySilence(channel, prefix, formatOptions):
+	fifo = os.open(prefix + str(channel), os.O_WRONLY)
+	soxPlayers[channel] = subprocess.Popen([itm for lst in [["sox", "-n"], soxFormatOptions, ["-p", "repeat", "-"]] for itm in lst], stdout=fifo)
+
+
 #### #### #### #### 
 
 usage = "soxplayer.py infile1 [infile2]..."
@@ -51,6 +62,11 @@ mkPipes(loopFifoPrefix, nbLoops)
 
 soxFileOptions = [itm for lst in [opt for i in range(0, nbLoops) for opt in soxFormatOptions, [loopFifoPrefix + str(i)]] for itm in lst]
 soxPlayer = subprocess.Popen([itm for lst in [["sox"], soxGlobalOptions, soxFileOptions, soxOutfile] for itm in lst])
+
+soxPlayers = [ None ] * nbLoops
+thread.start_new_thread(soxPlaySilence, (0, loopFifoPrefix, soxFormatOptions))
+for i in range(1, len(sys.argv)):
+	thread.start_new_thread(soxPlayLoop, (i, loopFifoPrefix, sys.argv[i]))
 
 soxPlayer.wait()
 
